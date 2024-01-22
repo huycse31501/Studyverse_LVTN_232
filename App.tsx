5bc88ab26bc853fe4e85484e498e52e33f19a83b
huycse31501 <quanghuy31501@gmail.com>
2024-01-22 19:58:25 +0700

    chore: adding text input component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TextInputField from './component/signin-signup/TextInputField';
-
-
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [fontsLoaded, setFontsLoaded] = useState<boolean>(false);
+
+    useEffect(() => {
+      async function loadFonts() {
+        try {
+          await SplashScreen.preventAutoHideAsync();
+          await Font.loadAsync({
+            'roboto': require('./assets/fonts/Roboto-Regular.ttf'),
+          });
+        } catch (e) {
+          console.warn('Error loading fonts', e);
+        } finally {
+          setFontsLoaded(true);
+          await SplashScreen.hideAsync();
+        }
+      }
+
+      loadFonts();
+    }, []);
+
+    if (!fontsLoaded) {
+      return null; // Or a loading indicator of your choice
+    }
+
   return (
     <>
       <StatusBar style='dark' />
         <View style={styles.container}>
-          <TextInputField placeHolder='Email' />
+          <TextInputField placeHolder='Email' required />
         </View>
     </>
   );
--- component/signin-signup/TextInputField.tsx
+++ component/signin-signup/TextInputField.tsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import TextInputFieldProps from '../type/TextInputField';
-
-
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import TextInputFieldProps from "../type/TextInputField";
 
 const InputField = ({ placeHolder, required }: TextInputFieldProps) => {
-  
+  const [isFocused, setIsFocused] = useState(false);
+  const [value, setValue] = useState("");
+
+  // Function to handle the focus state
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
 
   return (
     <View style={styles.container}>
+      {!value && !isFocused && (
+        <Text style={styles.placeholder}>
+          {placeHolder}
+          {required && <Text style={styles.asterisk}> *</Text>}
+        </Text>
+      )}
       <TextInput
-        placeholder={placeHolder}
-        style={styles.input}
+        style={[
+          styles.input,
+          !value && !isFocused ? styles.inputPlaceholder : null,
+        ]}
+        value={value}
+        onChangeText={setValue}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         autoCorrect={false}
       />
     </View>
   );
-}
-
-
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -28,12 +40,26 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: 'black',
     marginVertical: 20,
+    position: 'relative',
   },
   input: {
     fontSize: 18,
+    color: 'black', 
+  },
+  inputPlaceholder: {
+    color: 'transparent',
+  },
+  placeholder: {
+    position: 'absolute',
+    left: 10,
+    bottom: 10,
+    fontSize: 50,
+    color: 'grey',
+    fontFamily: 'roboto',
+  },
+  asterisk: {
+    color: 'red',
   },
 });
 
-
-
-export default InputField
+export default InputField;
