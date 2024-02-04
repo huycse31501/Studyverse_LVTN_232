128f980641c4e3ea5039dedb1c570c3efeebc0b0
huycse31501 <quanghuy31501@gmail.com>
2024-02-04 15:09:55 +0700

    feat: password forgot component-p2

--- component/navigator/appNavigator.tsx
+++ component/navigator/appNavigator.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import SignIn from "../../screen/SignIn-SignUp/SignIn";
 import SignUp from "../../screen/SignIn-SignUp/SignUp";
 import { StyleSheet } from "react-native";
+import ForgotPassword from "../../screen/SignIn-SignUp/ForgotPassword";
 
 const Stack = createStackNavigator();
 
@@ -10,12 +11,13 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="SignUp"
+        initialRouteName="SignIn"
         screenOptions={{
           headerShown: false,
           cardStyle: { backgroundColor: "#FFFFFF" },
         }}
       >
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
         <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="SignIn" component={SignIn} />
       </Stack.Navigator>
@@ -23,9 +25,4 @@ const AppNavigator = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#ffffff",
-  },
-});
 export default AppNavigator;
